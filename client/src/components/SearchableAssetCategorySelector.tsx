b2e47c7e8a76e7a0346253641e719202cc02ed20
cOsMiCTr <onurbaki@me.com>
2025-10-27 10:44:12 +0100

    Increase asset category dropdown height to 180px to show 4 full items comfortably

--- client/src/components/SearchableAssetCategorySelector.tsx
+++ client/src/components/SearchableAssetCategorySelector.tsx
@@ -155,7 +155,7 @@ const SearchableAssetCategorySelector: React.FC<SearchableAssetCategorySelectorP
           </div>
 
           {/* Categories List */}
-          <div className="max-h-40 overflow-y-auto" style={{ maxHeight: '160px' }}>
+          <div className="overflow-y-auto" style={{ maxHeight: '180px' }}>
             {filteredCategories.length > 0 ? (
               filteredCategories.map((category) => {
                 const IconComponent = getCategoryIcon(category.icon || 'CubeTransparentIcon');
